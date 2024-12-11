55c671afea81d52c41dc2bd2b5a6f69d391b566c
xuyuchen <yu3hen@126.com>
2024-12-11 08:24:19 +0800

    修改作业项点，作业内容展示。

--- lib/routes/production/TaskPackageDetailList.dart
+++ lib/routes/production/TaskPackageDetailList.dart
@@ -1,25 +1,70 @@
 import '../../index.dart';
+import 'package:flutter/material.dart';
 
 class TaskPackageDetailsPage extends StatelessWidget {
-  final dynamic package;
+    final dynamic package;
 
-  TaskPackageDetailsPage({required this.package});
+    TaskPackageDetailsPage({required this.package});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('作业项点'),
-      ),
-      body: ListView.builder(
-        itemCount: package.taskCertainPackageList.length,
-        itemBuilder: (context, index) {
-          final taskPackage = package.taskCertainPackageList[index];
-          return ListTile(
-            title: Text('${taskPackage.name}'),
-          );
-        },
-      ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('作业包详情'),
+            ),
+            body: ListView.builder(
+                itemCount: package.taskCertainPackageList.length,
+                itemBuilder: (context, index) {
+                    final taskPackage = package.taskCertainPackageList[index];
+                    return Container(
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.blue,
+                                width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                                Text(
+                                    '${taskPackage.name}',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                    ),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                    '风险等级: ${taskPackage.riskLevel?? '无'}', // 假设taskPackage里有riskLevel字段来表示风险等级，若没有则显示'无'
+                                    style: TextStyle(fontSize: 16),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                    '状态: ${taskPackage.complete?? '未知'}', // 假设taskPackage里有status字段来表示状态，若没有则显示'未知'
+                                    style: TextStyle(fontSize: 16),
+                                ),
+                                SizedBox(height: 16),
+                                ListTile(
+                                    title: Text('点击查看作业内容'),
+                                    onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => TaskContentDetailsPage(
+                                                    taskInstructContentList: taskPackage.taskInstructContentList, // 传递当前作业项对应的作业内容列表
+                                                ),
+                                            ),
+                                        );
+                                    },
+                                ),
+                            ],
+                        ),
+                    );
+                },
+            ),
+        );
+    }
 }
---
+++ lib/routes/production/taskContentDetailsPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TaskContentDetailsPage extends StatelessWidget {
+  final List<dynamic> taskInstructContentList;
+
+  TaskContentDetailsPage({required this.taskInstructContentList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('作业内容详情'),
+      ),
+      body: ListView.separated(
+        itemCount: taskInstructContentList.length,
+        separatorBuilder: (BuildContext context, int index) => Divider(
+          color: Colors.grey.shade300,
+          height: 1,
+        ),
+        itemBuilder: (context, index) {
+          final contentItem = taskInstructContentList[index];
+          return ListTile(
+            title: Text(
+              contentItem.name?? '无作业内容',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+          );
+        },
+      ),
+    );
+  }
+}
